b1198419073aa392aad5ffd5095cb2e9b86a168b
SamSmo <samsmolenski@gmail.com>
2012-04-17 13:18:34 -0400

    rudimentary bone to openNi sync implemented

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -2,19 +2,27 @@
 
 //--------------------------------------------------------------
 void testApp::setup() {
+    ofDisableArbTex();
     ofSetFrameRate(60);
     ofSetSmoothLighting(true);
 	light.setAmbientColor(ofColor(.0, .0, .0));
 	light.setDiffuseColor(ofColor(.0, .0, .0));
 	light.setSpecularColor(ofColor(255, .1, .1));
-    ofDisableArbTex();
     ofSetLogLevel(OF_LOG_NOTICE);
     
-    if(model.loadModel("testmonster.dae",true) && arm.loadModel("leg.dae",true)){
+    if(model.loadModel("astroBoy_walk.dae",true)){
         model.calculateDimensions();
+        material = model.getMaterialForMesh(0);
+        mSetup = true;
+        model.setPosition(ofGetWidth() / 2, ofGetHeight() * 0.75, 0);  
     }
     
     ofEnableSmoothing();
+    glEnable(GL_DEPTH_TEST);  
+	glShadeModel(GL_SMOOTH);
+    ofEnableSeparateSpecularLight(); 
+    pHeadY = 0;
+    pHeadX = 0;
     openNIDevice.setup();
     //openNIDevice.setLogLevel(OF_LOG_VERBOSE);
     openNIDevice.addDepthGenerator();
@@ -38,6 +46,26 @@ void testApp::setup() {
 void testApp::update(){
     ofBackground(0, 0, 0);
     openNIDevice.update();
+    
+    int numUsers = openNIDevice.getNumTrackedUsers();
+    if(numUsers > 0){
+        ofxOpenNIUser & user = openNIDevice.getTrackedUser(0);
+        
+        _HEAD = user.getJoint(JOINT_HEAD).getProjectivePosition();
+        _ARM = user.getJoint(JOINT_LEFT_FOOT).getProjectivePosition();
+        
+        model.translateBone(model.getAssimpScene()->mMeshes[0]->mBones[2], 0.0, 5*(pHeadY- int(_HEAD.y))/float(ofGetHeight()), -5*(pHeadX-int(_HEAD.x))/float(ofGetWidth()));
+        
+        pMouseX = mouseX;
+        pMouseY = mouseY;
+        
+        pHeadY = _HEAD.y;
+        pHeadX = _HEAD.x;
+        //pMouseZ = mouseZ;
+    }
+		//	model.rotateBone(model.getAssimpScene()->mMeshes[cualMesh]->mBones[cualBone], 0.0, 5*(pMouseY- mouseY)/float(ofGetHeight()), 5*(pMouseX-mouseX)/float(ofGetWidth()));
+		
+	model.updateMeshes();
 }
 
 //--------------------------------------------------------------
@@ -53,29 +81,32 @@ void testApp::draw(){
     openNIDevice.drawDepth(0, 0, 0, 0);
     openNIDevice.drawSkeletons(0,0,640*2,540*2);
 
-    int numUsers = openNIDevice.getNumTrackedUsers();
+   /* int numUsers = openNIDevice.getNumTrackedUsers();
     if(numUsers > 0){
         ofxOpenNIUser & user = openNIDevice.getTrackedUser(0);
         
             _HEAD = user.getJoint(JOINT_HEAD).getProjectivePosition();
             _ARM = user.getJoint(JOINT_LEFT_FOOT).getProjectivePosition();
         
+      */  
         //tranlation code for different limbs / models
-        glPushMatrix();
+       /* glPushMatrix();
             glTranslatef(_HEAD.x, _HEAD.y, -_HEAD.z);
             ofRotateY(90);
-            model.drawFaces();
+            material.begin();
+                model.drawFaces();
+            material.end();
         glPopMatrix();
         
         glPushMatrix();
             glTranslatef(_ARM.x,_ARM.y, -_ARM.z);
             ofRotateY(90);
-            arm.drawFaces();
+           // arm.drawFaces();
         glPopMatrix();
-           
+           */
     
-    } 
-
+    //} 
+    model.drawFaces();
     light.disable();
 	glDisable(GL_DEPTH_TEST);
 	ofDisableLighting();
@@ -94,6 +125,11 @@ void testApp::keyPressed(int key){
         case 'x':
             openNIDevice.stop();
             break;
+        case 'y':
+            cout << 5*(pMouseY- mouseY)/float(ofGetHeight())<<"mouse Y"<<endl;
+            cout << 5*(pHeadY - _HEAD.y)/float(ofGetHeight())<<"head Y"<<endl;
+            break;
+        
     }
 }
 
@@ -127,3 +163,96 @@ void testApp::windowResized(int w, int h){
 
 }
 
+void SkeletonDrawer::translateBone(aiBone *bone, float x, float y, float z){
+	
+	aiNode *node = scene->mRootNode->FindNode(bone->mName);
+	
+	aiMatrix4x4 mat;
+	aiMatrix4x4::Translation(aiVector3D(x,y,z), mat);
+	node->mTransformation*=mat;
+	
+}
+void SkeletonDrawer::rotateBone(aiBone *bone, float x, float y, float z){
+	
+	aiNode *node = scene->mRootNode->FindNode(bone->mName);
+	
+	aiMatrix4x4 mat;
+	aiMatrix4x4::RotationX(x, mat);
+	node->mTransformation*=mat;
+	
+	aiMatrix4x4::RotationY(y, mat);
+	node->mTransformation*=mat;
+	
+	aiMatrix4x4::RotationZ(z, mat);
+	node->mTransformation*=mat;
+} 
+
+//The following is just a copy of the second part of ofxAssimpModelLoader::updateAnimation method.
+//I just copyed it just for not modifiying the assimp addon and to lazy to extend the class.
+void SkeletonDrawer::updateMeshes(){  
+    // update mesh position for the animation
+    for (unsigned int i = 0; i < modelMeshes.size(); ++i){
+        
+        // current mesh we are introspecting
+        const aiMesh* mesh = modelMeshes[i].mesh;
+        
+        // calculate bone matrices
+        std::vector<aiMatrix4x4> boneMatrices( mesh->mNumBones);
+        for( size_t a = 0; a < mesh->mNumBones; ++a)
+        {
+            const aiBone* bone = mesh->mBones[a];
+            
+            // find the corresponding node by again looking recursively through the node hierarchy for the same name
+            aiNode* node = scene->mRootNode->FindNode(bone->mName);
+            
+            // start with the mesh-to-bone matrix
+            boneMatrices[a] = bone->mOffsetMatrix;
+            // and now append all node transformations down the parent chain until we're back at mesh coordinates again
+            const aiNode* tempNode = node;
+            while( tempNode)
+            {
+                // check your matrix multiplication order here!!!
+                boneMatrices[a] = tempNode->mTransformation * boneMatrices[a];
+                // boneMatrices[a] = boneMatrices[a] * tempNode->mTransformation;
+                tempNode = tempNode->mParent;
+            }
+            modelMeshes[i].hasChanged = true;
+            modelMeshes[i].validCache = false;
+        }
+        
+        modelMeshes[i].animatedPos.assign(modelMeshes[i].animatedPos.size(),0);
+        if(mesh->HasNormals()){
+            modelMeshes[i].animatedNorm.assign(modelMeshes[i].animatedNorm.size(),0);
+        }
+        // loop through all vertex weights of all bones
+        for( size_t a = 0; a < mesh->mNumBones; ++a)
+        {
+            const aiBone* bone = mesh->mBones[a];
+            const aiMatrix4x4& posTrafo = boneMatrices[a];
+            
+            
+            for( size_t b = 0; b < bone->mNumWeights; ++b)
+            {
+                const aiVertexWeight& weight = bone->mWeights[b];
+                
+                size_t vertexId = weight.mVertexId;
+                const aiVector3D& srcPos = mesh->mVertices[vertexId];
+                
+                modelMeshes[i].animatedPos[vertexId] += weight.mWeight * (posTrafo * srcPos);
+            }
+            if(mesh->HasNormals()){
+                // 3x3 matrix, contains the bone matrix without the translation, only with rotation and possibly scaling
+                aiMatrix3x3 normTrafo = aiMatrix3x3( posTrafo);
+                for( size_t b = 0; b < bone->mNumWeights; ++b)
+                {
+                    const aiVertexWeight& weight = bone->mWeights[b];
+                    size_t vertexId = weight.mVertexId;
+                    
+                    const aiVector3D& srcNorm = mesh->mNormals[vertexId];
+                    modelMeshes[i].animatedNorm[vertexId] += weight.mWeight * (normTrafo * srcNorm);
+                    
+                }
+            }
+        }
+    }
+}
--- src/testApp.h
+++ src/testApp.h
@@ -6,14 +6,36 @@
 #include "ofVboMesh.h"
 #include "ofMain.h"
 
-class testApp : public ofBaseApp{
+class SkeletonDrawer : public ofxAssimpModelLoader  
+{  
+public:  
+	void drawSkeleton();  
+	void rotateBone(aiBone *bone, float x, float y, float z);
+	void translateBone(aiBone *bone, float x, float y, float z);
+	void updateMeshes();
+	int cualBone;
+	string *trafoMsg;
+	void randomPose();
+private:  
+	std::map<aiNode*, aiMatrix4x4> nodeMatrices;  
+	void collectNodes(const struct aiScene *sc, const struct aiNode *nd);  
+	
+}; 
 
+class testApp : public ofBaseApp{
+private:
+    void listBoneNames();
+    void buildBoneNameList();
+    
 public:
     
 	void setup();
 	void update();
 	void draw();
     void exit();
+    const vector<string>& getBoneNames();
+    const aiScene* getAiScene(ofxAssimpModelLoader model);
+    aiBone* getBoneNamed(aiMesh *mesh, string boneName);
     
 	void keyPressed  (int key);
 	void keyReleased(int key);
@@ -30,8 +52,8 @@ public:
     ofPoint _HEAD;
     ofPoint _ARM;
     
-    ofxAssimpModelLoader model;
-    ofxAssimpModelLoader arm;
+    SkeletonDrawer model;
+    //ofxAssimpModelLoader arm;
     
     ofVboMesh mesh;
     ofPoint position;
@@ -46,6 +68,20 @@ public:
     ofCamera					camera;
 	float						camLat, camLon, camDist;
 	ofVec3f						camTarget;
+    
+    /*
+     *  List of bone names  string( mBones[ j ]->mName.data )
+     */
+    vector<string>                              mBoneNames;
+    /*
+     *  Bool to check if bone name lists are ready or not.
+     */
+    bool                                        mBoneListCreated;
+    
+    bool mSetup;
+    
+    int pMouseY, pMouseX, pMouseZ;
+    int pHeadY, pHeadX, pHeadZ;
     
 };
 
